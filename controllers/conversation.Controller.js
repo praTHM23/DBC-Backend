8060e938afb79a356ef418bb1609e7d336bd42c6
“finsoncoutinho” <finsoncoutinho2125@gmail.com>
2022-11-03 19:29:59 +0530

    get

--- controllers/conversation.Controller.js
+++ controllers/conversation.Controller.js
@@ -2,29 +2,27 @@ const Conversation = require("../models/conversation");
 const express = require("express");
 const conversationRouter = express.Router();
 
-conversationRouter.post("/", async(req, res) => {
+conversationRouter.post("/", async (req, res) => {
+  const newConversation = new Conversation({
+    members: [req.body.senderId, req.body.receiverId],
+  });
 
-    const newConversation = new Conversation({
-        members: [req.body.senderId, req.body.receiverId],
-      });
-    
-      try {
-        const savedConversation = await newConversation.save();
-        res.status(200).json(savedConversation);
-      } catch (err) {
-        res.status(500).json(err);
-      }
+  try {
+    const savedConversation = await newConversation.save();
+    res.status(200).json(savedConversation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-conversationRouter.get("/", async(req, res) => {
-
-    try {
-        const conversation = await Conversation.find({
-          members: { $in: [req.params.userId] },
-        });
-        res.status(200).json(conversation);
-      } catch (err) {
-        res.status(500).json(err);
-      }
+conversationRouter.get("/:id", async (req, res) => {
+  try {
+    const conversation = await Conversation.find({
+      members: { $in: [req.params.userId] },
+    });
+    res.status(200).json(conversation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-module.exports= conversationRouter;
+module.exports = conversationRouter;
